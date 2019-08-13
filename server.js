afeb92d594ef581a5ba72b2d56934a75962a2e48
angeloDiepe <angelodiepe10@yahoo.com>
2019-08-13 07:13:15 +0100

    basic req/res done

--- server.js
+++ server.js
@@ -1,59 +1,77 @@
 const express=require('express');
-const app=express();
 const bodyParser=require('body-parser');
+const cors=require('cors');
+const knex=require('knex');
+const bcrypt = require('bcryptjs');
+const app=express();
 
-const db={
-    users:[
-        {
-            id:001,
-            name:"John Doe",
-            email:"johndoe@yahoo.com",
-            password:"johndoe",
-            joined:new Date(),
-        },
-        {
-            id: 002,
-            name: "Ann Marie",
-            email: "annmarie@yahoo.com",
-            password: "annmarie",
-            joined: new Date(),
-        },
-        {
-            id: 003,
-            name: "Sally Dowson",
-            email: "sallydowson@yahoo.com",
-            password: "sallydowson",
-            joined: new Date(),
-        },
-    ]
-}
-
+const db=knex({
+    client:'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'Angelo2001',
+        database:'markup-doc-db'
+    }
+});
+//middleware
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.json(db.users)
+//request/response
+app.get("/:cat/:recent",(req,res)=>{
+    console.log(req.params)
+    if(req.params.recent==="recent"){
+        db.select('category', 'domain', 'question', 'answer', 'date', 'name').from("qna").join("users", 'users.email', '=', 'qna.email').where({ category: req.params.cat }).orderBy('date','desc')
+            .then(qna => res.json(qna))
+            .catch(err => res.json('something went wrong'))
+    }else{
+    db.select('category','domain','question','answer','date','name').from("qna").join("users",'users.email','=','qna.email').where({category:req.params.cat})
+    .then(qna=>res.json(qna))
+    .catch(err=>res.json('something went wrong'))
+    }
 })
 
-app.get("/signin",(req,res)=>{
+app.post("/signin",(req,res)=>{
     let {email,password}=req.body;
-   if(email===db.users[0].email && password===db.users[0].password){
-       res.json("successful signin");
-    }else{
-           res.json("wrong credentials")
-    }
-    
+   db.select("*").from("login").where("email","=",email)
+   .then(user=>{
+       let isValid=bcrypt.compareSync(password,user[0].hash);
+       if(isValid){
+           return db.select("*").from("users").where("email","=",email)
+                    .then(user=>res.json(user[0]))
+                    .catch(err=>res.status(400).json("wrong credentials"))
+       }else{
+           return res.status(400).json("wrong credentials");
+       }
+   })
+   .catch(err=>res.json("wrong credentials"))
 })
 
 app.post("/register",(req,res)=>{
-const {name,email,password}=req.body
-    db.users.push({
-        id:004,
-        name:name,
-        email:email,
-        password:password,
-        joined:new Date(),
-    });
-        res.json("registered");
+let {name,email,password}=req.body;
+let hash=bcrypt.hashSync(password);
+    db.transaction(trx=>{
+        trx.insert({
+            hash:hash,
+            email:email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail=>{
+            return trx('users')
+            .insert({
+                name: name.toLowerCase(),
+                email: loginEmail[0],
+                joined: new Date()
+           })
+            .returning("*")
+            .then(user => res.json(user[0]))
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json("unable to register"));
 })
 
 app.get("/search",(req,res)=>{
@@ -61,8 +79,17 @@ app.get("/search",(req,res)=>{
     res.json("searching...")
 })
 
-
-
+app.post("/add",(req,res)=>{
+    let  {category,domain,question,answer,email,tags,date}=req.body;
+    tags = tags.reduce((acc, el) => { return acc + "," + el }, "").replace(',', "");
+    db.insert({
+        category, domain, question, answer, email, date, tags
+    }).into("qna")
+    .returning("*")
+    .then(qna => res.json("added"))
+    .catch(err=>res.status(400).json("unable to add"))
+   
+})
 
 app.listen(3001,()=>{
     console.log("app is running on port 3001")
